da34443af1a57c55fa667d39788edef409958d17
Anthony33720 <raynaud.anthony@gmail.com>
2024-10-17 10:58:08 +0300

    final initialization

--- src/main/kotlin/org/example/articlekotlin/ArticleKotlinApplication.kt
+++ src/main/kotlin/org/example/articlekotlin/ArticleKotlinApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class ArticleKotlinApplication
 
 fun main(args: Array<String>) {
-    runApplication<ArticleKotlinApplication>(*args)
+	runApplication<ArticleKotlinApplication>(*args)
 }
--- src/test/kotlin/org/example/articlekotlin/ArticleKotlinApplicationTests.kt
+++ src/test/kotlin/org/example/articlekotlin/ArticleKotlinApplicationTests.kt
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class ArticleKotlinApplicationTests {
 
-    @Test
-    fun contextLoads() {
-    }
+	@Test
+	fun contextLoads() {
+	}
 
 }
