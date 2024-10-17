6e640842602f778467537fe2bf72a9bf78a9aaac
Anthony33720 <raynaud.anthony@gmail.com>
2024-10-17 10:26:37 +0300

    Initial commit - Article Rest spring

---
+++ src/main/kotlin/org/example/articlekotlin/ArticleKotlinApplication.kt
@@ -0,0 +1,11 @@
+package org.example.articlekotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArticleKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<ArticleKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/articlekotlin/ArticleKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.articlekotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArticleKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
