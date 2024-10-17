5904d7a948f27fc913becd1cd4e8bbabeda71044
Anthony33720 <raynaud.anthony@gmail.com>
2024-10-17 12:50:13 +0300

    updated crud end point with 'slug'

--- src/main/kotlin/org/example/articlekotlin/Article.kt
+++ src/main/kotlin/org/example/articlekotlin/Article.kt
@@ -3,9 +3,9 @@ package org.example.articlekotlin
 import java.time.LocalDateTime
 
 data class Article(
-        val title : String,
-        val content : String?,
-        val createdAt: LocalDateTime = LocalDateTime.now(),
-        val slug: String = title.toSlug()
+        var title : String,
+        var content : String?,
+        var createdAt: LocalDateTime = LocalDateTime.now(),
+        var slug: String = title.toSlug()
 )
 
--- src/main/kotlin/org/example/articlekotlin/RestControllers.kt
+++ src/main/kotlin/org/example/articlekotlin/RestControllers.kt
@@ -6,16 +6,13 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/articles")
-class ArticleController{
-        val articles: MutableList<Article> = mutableListOf(Article("My title",
-                "my content"))
+class ArticleController {
 
-        @GetMapping
-        fun articles() = articles
-
+        val articles: MutableList<Article> = mutableListOf(Article("My title", "my content"))
 
         @GetMapping
         fun getAllArticles(): List<Article> = articles
+
 
         @PostMapping
         fun createArticle(@RequestBody article: Article): ResponseEntity<String> {
@@ -23,29 +20,33 @@ class ArticleController{
                 return ResponseEntity("Article created successfully", HttpStatus.CREATED)
         }
 
-        @GetMapping("/{index}")
-        fun getArticleById(@PathVariable index: Int): ResponseEntity<Any> {
-                return if (index < articles.size) {
-                        ResponseEntity(articles[index], HttpStatus.OK)
+        @GetMapping("/{slug}")
+        fun getArticleBySlug(@PathVariable slug: String): ResponseEntity<Any> {
+                val article = articles.find { it.slug == slug }
+                return if (article != null) {
+                        ResponseEntity(article, HttpStatus.OK)
                 } else {
                         ResponseEntity("Article not found", HttpStatus.NOT_FOUND)
                 }
         }
 
-        @PutMapping("/{index}")
-        fun updateArticle(@PathVariable index: Int, @RequestBody updatedArticle: Article): ResponseEntity<String> {
-                return if (index < articles.size) {
-                        articles[index] = updatedArticle
+
+        @PutMapping("/{slug}")
+        fun updateArticle(@PathVariable slug: String, @RequestBody updatedArticle: Article): ResponseEntity<String> {
+                val index = articles.indexOfFirst { it.slug == slug }
+                return if (index != -1) {
+                        articles[index] = updatedArticle.copy(slug = updatedArticle.title.toSlug())
                         ResponseEntity("Article updated successfully", HttpStatus.OK)
                 } else {
                         ResponseEntity("Article not found", HttpStatus.NOT_FOUND)
                 }
         }
 
-        @DeleteMapping("/{index}")
-        fun deleteArticle(@PathVariable index: Int): ResponseEntity<String> {
-                return if (index < articles.size) {
-                        articles.removeAt(index)
+        @DeleteMapping("/{slug}")
+        fun deleteArticle(@PathVariable slug: String): ResponseEntity<String> {
+                val article = articles.find { it.slug == slug }
+                return if (article != null) {
+                        articles.remove(article)
                         ResponseEntity("Article deleted successfully", HttpStatus.OK)
                 } else {
                         ResponseEntity("Article not found", HttpStatus.NOT_FOUND)
